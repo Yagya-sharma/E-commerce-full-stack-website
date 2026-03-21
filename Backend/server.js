0b34e9a2c516a518ac211d2818ce9701159eccd7
Yagya sharma <sharmayagya74@gmail.com>
2026-03-21 22:37:37 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,9 @@ const PORT=process.env.PORT || 3000
 app.use(express.json())
 app.use(cors({
     origin:process.env.FRONTEND_URL,
-    credentials:true
+    credentials:true,
+    methods:['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders:['Content-Type','Authorization']
 }))
 
 app.use('/api/v1/user',userRoute)
