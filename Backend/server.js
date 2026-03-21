e7700ac0af0733873a6083379aa92a47e0f34870
Yagya sharma <sharmayagya74@gmail.com>
2026-03-22 02:18:16 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -12,12 +12,33 @@ const PORT=process.env.PORT || 3000
 
 //middleware
 app.use(express.json())
+// app.use(cors({
+//     origin:process.env.FRONTEND_URL,
+//     credentials:true,
+//     methods:['GET','POST','PUT','DELETE','OPTIONS'],
+//     allowedHeaders:['Content-Type','Authorization']
+// }))
+
+const allowedOrigins = [
+  "http://localhost:5173", // local dev
+  "https://e-commerce-frontend-9tod.onrender.com" // deployed frontend
+];
+
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    credentials:true,
-    methods:['GET','POST','PUT','DELETE','OPTIONS'],
-    allowedHeaders:['Content-Type','Authorization']
-}))
+  origin: function(origin, callback){
+    // allow requests with no origin like Postman
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+
 
 app.use('/api/v1/user',userRoute)
 app.use('/api/v1/product',productRoute)
